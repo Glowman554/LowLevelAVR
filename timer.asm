76c4ed00f3fc0f359dfedbb19fb98569433c7f65
Glowman554 <glowman434@gmail.com>
2022-07-23 16:53:38 +0000

    added comments to timer.asm

--- timer.asm
+++ timer.asm
@@ -24,35 +24,35 @@
 .org 0x0000 ; Execution starts here
     jmp main
 
-.org 0x001A
+.org 0x001A ; interrupt handler for timer 1 overflow
     jmp timer1_ovf_vect
 
 timer1_ovf_vect:
     push r16
     push r17
     push r18
-    
-    in r16, SREG
-    
+
+    in r16, SREG ; save SREG since it can happen that we interrupt the cpu at a point where the vaule in SREG is still needed
+
     in r17, PORTB
     ldi r18, 0b00100000
     eor r17, r18
-    out PORTB, r17
+    out PORTB, r17 ; flip PB5
 
-    call set_tcnt
+    call set_tcnt ; reset timer counter
 
-    out SREG, r16
+    out SREG, r16 ; restore sreg
     pop r18
     pop r17
     pop r16
-    reti
+    reti ; return from interrupt
 
 main:
     ldi r16, low(RAMEND)
     out SPL, r16 ; setup stack ptr low
     ldi r16, high(RAMEND)
     out SPH, r16 ; setu stack ptr high
-    
+
     rcall setup ; call setup
 
 loop_intrnl:
@@ -62,56 +62,56 @@ loop_intrnl:
 setup:
     sbi DDRB, LED_B ; set PB5 output
 
-    
-   call set_tcnt
+
+    call set_tcnt ; set timer counter
 
 
     ldi r16, 0b00000101
 
-    ldi r30, low(TCCR1B)
-    ldi r31, high(TCCR1B)
+    ldi r30, low(TCCR1B) ; set ZL
+    ldi r31, high(TCCR1B) ; set ZH
 
-    st Z, r16
+    st Z, r16 ; set preescaler to 1024
 
 
     ldi r16, 0
 
-    ldi r30, low(TCCR1A)
-    ldi r31, high(TCCR1A)
+    ldi r30, low(TCCR1A) ; set ZL
+    ldi r31, high(TCCR1A) ; set ZH
 
-    st Z, r16
+    st Z, r16 ; disable all timer features with makes it a overflow timer
 
 
     ldi r16, 0b00000001
 
-    ldi r30, low(TIMSK1)
-    ldi r31, high(TIMSK1)
+    ldi r30, low(TIMSK1) ; set ZL
+    ldi r31, high(TIMSK1) ; set ZH
 
-    st Z, r16
+    st Z, r16 ;  // unmask timer overflow interrupt 1
 
-    sei
+    sei ; enable interrupts
 
-    ret ; return 
+    ret ; return
 
 set_tcnt:
     push r16
     push r30
     push r31
-    
+
     ldi r16, low(TCNT_O)
 
-    ldi r30, low(TCNT1L)
-    ldi r31, high(TCNT1L)
+    ldi r30, low(TCNT1L) ; set ZL
+    ldi r31, high(TCNT1L) ; set ZH
 
-    st Z, r16
+    st Z, r16 ; set low part of timer counter
 
 
     ldi r16, high(TCNT_O)
 
-    ldi r30, low(TCNT1H)
-    ldi r31, high(TCNT1H)
+    ldi r30, low(TCNT1H) ; set ZL
+    ldi r31, high(TCNT1H) ; set ZH
 
-    st Z, r16
+    st Z, r16 ; set high part of timer counter
 
     pop r31
     pop r30
